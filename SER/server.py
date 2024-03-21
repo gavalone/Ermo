bb116a0e53ec5c6fd903a728f13a4fce5e29b18c
NasNasNas7631 <ananseregina@mail.ru>
2024-03-22 01:04:19 +0300

    ikujhgf

--- SER/server.py
+++ SER/server.py
@@ -1,3 +1,4 @@
+"""
 import socket
 
 HOST = '127.0.0.1'                 # Symbolic name meaning all available interfaces
@@ -12,3 +13,13 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
             data = conn.recv(1024)
             if not data: break
             conn.sendall(data)
+"""
+import socket
+
+import socket, cv2, pickle, struct, imutils
+
+
+server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+host_name = socket.gethostname()
+host_ip = socket.gethostbyname(host_name)
+print(f'HOST IP: {host_ip}')
